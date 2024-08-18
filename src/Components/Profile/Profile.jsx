5b95c82a2f1899b72cff691169b3a83475b32ec0
mdolidehasan <olidehasan444@gmail.com>
2024-08-18 17:52:20 +0600

    User profile added

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,20 +1,20 @@
-// import React, { useContext } from 'react';
-// import { AuthContext } from '../../Providers/AuthProviders';
-// import Navbar from '../Navbar/Navbar';
-// import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Providers/AuthProviders';
+import Navbar from '../Navbar/Navbar';
+import { Link } from 'react-router-dom';
 
-// const Profile = () => {
+const Profile = () => {
 
-//     const { user } = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
-//     return (
-//         <div className='mx-auto p-6'>
-//             <h3 className='text-center text-2xl font-bold mb-4'>Profile</h3>
-//             <img className='mx-auto mb-3' src={user.photoURL} alt="" />
-//             <p className='text-center mb-3'>Name: {user.displayName}</p>
-//             <p className='text-center'>back to <span className='text-blue-600'><Link to="/">home</Link></span></p>
-//         </div>
-//     );
-// };
+    return (
+        <div className='mx-auto p-6'>
+            <h3 className='text-center text-2xl font-bold mb-4'>Profile</h3>
+            <img className='mx-auto mb-3' src={user.photoURL} alt="" />
+            <p className='text-center mb-3'>Name: {user.displayName}</p>
+            <p className='text-center'>back to <span className='text-blue-600'><Link to="/">home</Link></span></p>
+        </div>
+    );
+};
 
-// export default Profile;
+export default Profile;
